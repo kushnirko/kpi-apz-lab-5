f2159cfbaef8d2d0474ca19eea059787da3eb40f
Mykola Kushnir <mykola.kushnir.825@gmail.com>
2024-06-05 07:27:04 +0300

    Refactor request handlers

--- cmd/db/database.go
+++ cmd/db/database.go
@@ -6,9 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/kushnirko/kpi-apz-lab-5/datastore"
@@ -43,40 +41,29 @@ func createDirectory() (string, error) {
 	}
 }
 
-func parseUrl(url *url.URL) (k, t string, err error) {
-	pathParts := strings.Split(url.EscapedPath(), "/")
-	query := url.Query()
-	if len(pathParts) != 3 || len(query) > 1 {
-		return "", "", fmt.Errorf("invalid url: %s", url)
-	}
-
-	k = pathParts[2]
-	t = query.Get("type")
-	if t == "" {
-		t = "string"
-	}
-	return
-}
-
 type responseBody struct {
 	Key   string `json:"key"`
 	Value any    `json:"value"`
 }
 
 type requestBody struct {
-	Value string `json:"value"`
+	Value any `json:"value"`
 }
 
 func handleGetRequest(rw http.ResponseWriter, r *http.Request, db *datastore.Db) {
-	k, t, err := parseUrl(r.URL)
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusBadRequest)
+	pathParts := strings.Split(r.URL.EscapedPath(), "/")
+	if len(pathParts) != 3 {
+		http.Error(rw, "invalid url path", http.StatusBadRequest)
 		return
 	}
 
+	k := pathParts[2]
+	t := r.URL.Query().Get("type")
+
 	var v any
+	var err error
 	switch t {
-	case "string":
+	case "string", "":
 		v, err = db.Get(k)
 	case "int64":
 		v, err = db.GetInt64(k)
@@ -89,41 +76,45 @@ func handleGetRequest(rw http.ResponseWriter, r *http.Request, db *datastore.Db)
 		return
 	}
 
-	rw.Header().Set("content-type", "application/json")
+	rw.Header().Set("Content-Type", "application/json")
 	if err = json.NewEncoder(rw).Encode(responseBody{Key: k, Value: v}); err != nil {
 		http.Error(rw, "json encoding error", http.StatusInternalServerError)
 	}
 }
 
 func handlePostRequest(rw http.ResponseWriter, r *http.Request, db *datastore.Db) {
-	k, t, err := parseUrl(r.URL)
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusBadRequest)
+	pathParts := strings.Split(r.URL.EscapedPath(), "/")
+	if len(pathParts) != 3 {
+		http.Error(rw, "invalid url path", http.StatusBadRequest)
 		return
 	}
 
+	k := pathParts[2]
+
 	var rb requestBody
-	if err = json.NewDecoder(r.Body).Decode(&rb); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&rb); err != nil {
 		http.Error(rw, "json decoding error", http.StatusBadRequest)
 		return
 	}
 
-	switch t {
-	case "string":
-		err = db.Put(k, rb.Value)
-	case "int64":
-		var v int64
-		v, err = strconv.ParseInt(rb.Value, 10, 64)
-		if err == nil {
-			err = db.PutInt64(k, v)
+	var err error
+	switch v := rb.Value.(type) {
+	case string:
+		err = db.Put(k, v)
+	case float64:
+		if v == float64(int64(v)) {
+			err = db.PutInt64(k, int64(v))
+		} else {
+			err = fmt.Errorf("non-integer value")
 		}
 	default:
-		err = fmt.Errorf("invalid data type %s", t)
+		err = fmt.Errorf("unknown value type")
 	}
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	rw.WriteHeader(http.StatusCreated)
 }
 
